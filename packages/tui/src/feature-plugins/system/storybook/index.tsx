0cb7fcc49a22656eb9bf34a54b7567f2f6142873
Kit Langton <kit.langton@gmail.com>
2026-08-14 14:14:03 -0400

    chore(tui): remove turn flash scaffolding

--- packages/tui/src/feature-plugins/system/storybook/index.tsx
+++ packages/tui/src/feature-plugins/system/storybook/index.tsx
@@ -4,7 +4,6 @@ import { createSignal, For, type JSX } from "solid-js"
 import { StoryFooter } from "./footer"
 import { sessionTabsStory } from "./session-tabs"
 import { sessionLocationMissingStory } from "./session-location-missing"
-import { turnSummaryStory } from "./turn-summary"
 
 /**
  * A story is a full-screen, fixture-driven simulation of a real production component. Stories own
@@ -16,7 +15,7 @@ export type Story = {
   render: (context: Plugin.Context) => JSX.Element
 }
 
-const stories: Story[] = [sessionTabsStory, turnSummaryStory, sessionLocationMissingStory]
+const stories: Story[] = [sessionTabsStory, sessionLocationMissingStory]
 
 function Commands(props: { context: Plugin.Context }) {
   props.context.keymap.layer(() => ({
